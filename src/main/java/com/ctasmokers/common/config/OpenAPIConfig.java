18221bbf73ff16ad4158ea8503108d5eb80c047d
Logan Kulinski <lbkulinski@gmail.com>
2026-04-29 17:46:48 -0500

    Code cleanup

--- src/main/java/com/ctasmokers/common/config/OpenAPIConfig.java
+++ src/main/java/com/ctasmokers/common/config/OpenAPIConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,34 +14,25 @@ import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
-    private final OpenAPIProperties openAPIProperties;
-    private final BuildProperties buildProperties;
-
-    @Autowired
-    public OpenAPIConfig(
+    @Bean
+    public OpenAPI openAPI(
         OpenAPIProperties openAPIProperties,
         BuildProperties buildProperties
     ) {
-        this.openAPIProperties = openAPIProperties;
-        this.buildProperties = buildProperties;
-    }
-
-    @Bean
-    public OpenAPI openAPI() {
-        OpenAPIProperties.Contact contactProperties = this.openAPIProperties.contact();
+        OpenAPIProperties.Contact contactProperties = openAPIProperties.contact();
 
         Contact contact = new Contact()
             .name(contactProperties.name())
             .email(contactProperties.email());
 
-        OpenAPIProperties.License licenseProperties = this.openAPIProperties.license();
+        OpenAPIProperties.License licenseProperties = openAPIProperties.license();
 
         License apacheLicense = new License()
             .name(licenseProperties.name())
             .url(licenseProperties.url());
 
-        OpenAPIProperties.Info infoProperties = this.openAPIProperties.info();
-        String version = this.buildProperties.getVersion();
+        OpenAPIProperties.Info infoProperties = openAPIProperties.info();
+        String version = buildProperties.getVersion();
 
         Info apiInfo = new Info()
             .title(infoProperties.title())
@@ -51,7 +41,7 @@ public class OpenAPIConfig {
             .contact(contact)
             .license(apacheLicense);
 
-        OpenAPIProperties.Server serverProperties = this.openAPIProperties.server();
+        OpenAPIProperties.Server serverProperties = openAPIProperties.server();
 
         Server productionServer = new Server()
             .url(serverProperties.url())
--- src/main/java/com/ctasmokers/smoking/aggregate/model/SmokingReportAggregate.java
+++ src/main/java/com/ctasmokers/smoking/aggregate/model/SmokingReportAggregate.java
@@ -1,23 +1,23 @@
 package com.ctasmokers.smoking.aggregate.model;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Value;
 import org.jspecify.annotations.NullMarked;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbImmutable;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
-@Value
 @Builder
-@NullMarked
 @DynamoDbImmutable(builder = SmokingReportAggregate.SmokingReportAggregateBuilder.class)
-public class SmokingReportAggregate {
-    @Getter(onMethod_ = @DynamoDbPartitionKey)
-    String pk;
+@NullMarked
+public record SmokingReportAggregate(
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("PK")
+    String pk,
 
-    @Getter(onMethod_ = @DynamoDbSortKey)
-    String sk;
+    @DynamoDbSortKey
+    @DynamoDbAttribute("SK")
+    String sk,
 
-    long reportCount;
-}
+    long reportCount
+) {}
