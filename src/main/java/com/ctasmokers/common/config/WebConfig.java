0ea41adb46ff20e021db66127fd08823b0ee567d
Logan Kulinski <lbkulinski@gmail.com>
2026-05-11 22:37:53 -0500

    Update WebConfig to enable CORS configuration and fix MVC auto-configuration (#37)

--- src/main/java/com/ctasmokers/common/config/WebConfig.java
+++ src/main/java/com/ctasmokers/common/config/WebConfig.java
@@ -1,34 +1,31 @@
 package com.ctasmokers.common.config;
 
 import com.ctasmokers.common.config.properties.CorsProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
-@EnableWebMvc
-public final class WebConfig implements WebMvcConfigurer {
-    private final CorsProperties corsProperties;
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(CorsProperties corsProperties) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                String[] allowedOrigins = corsProperties.allowedOrigins()
+                                                        .toArray(String[]::new);
+                String[] allowedMethods = corsProperties.allowedMethods()
+                                                        .toArray(String[]::new);
+                String[] allowedHeaders = corsProperties.allowedHeaders()
+                                                        .toArray(String[]::new);
 
-    @Autowired
-    public WebConfig(CorsProperties corsProperties) {
-        this.corsProperties = corsProperties;
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        String[] allowedOrigins = this.corsProperties.allowedOrigins()
-                                                     .toArray(String[]::new);
-        String[] allowedMethods = this.corsProperties.allowedMethods()
-                                                     .toArray(String[]::new);
-        String[] allowedHeaders = this.corsProperties.allowedHeaders()
-                                                     .toArray(String[]::new);
-
-        registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods(allowedMethods)
-                .allowedHeaders(allowedHeaders);
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods(allowedMethods)
+                        .allowedHeaders(allowedHeaders);
+            }
+        };
     }
 }
